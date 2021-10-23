9ad2692b3ad1b668f866d07cd01e4c0fe08bd371
Hayabusha117 <62165295+Hayabusha117@users.noreply.github.com>
2021-10-23 02:39:36 -0500

    Practica5_Manuel_Chavez
    es la implementación del laberinto en Unity con todo y botones para desplazamiento

---
+++ Assets/scrips/NewMov.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMov : MonoBehaviour
+{
+
+    float speed = 35f;
+   
+    
+    // Start is called before the first frame update
+    
+
+    public void Muevete()
+    {
+        Vector3 movementInput = Vector3.zero;
+        movementInput.x = -1;
+        move(movementInput);
+    }
+
+    public void atras()
+    {
+        Vector3 movementInput = Vector3.zero;
+        movementInput.x = 1;
+        move(movementInput);
+    }
+
+    public void derecha()
+    {
+        Vector3 movementInput = Vector3.zero;
+        movementInput.z = 1;
+        move(movementInput);
+    }
+
+    public void izquierda()
+    {
+        Vector3 movementInput = Vector3.zero;
+        movementInput.z = -1;
+        move(movementInput);
+    }
+
+
+    void move(Vector3 dir)
+    {
+        transform.position += dir.normalized * speed * Time.deltaTime;
+    }
+
+}
